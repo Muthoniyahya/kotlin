af679b234fcfb84d648718be433a853664e453a6
student <Muthoniyahya@gmail.com>
2021-04-20 22:02:25 +0300

    first commit

---
+++ src/conditional statements.kt
@@ -0,0 +1,46 @@
+fun main(){
+    Even(6)
+    Even(3)
+    Even(25)
+    Even(12)
+ var  p=  Kibanda("Kales",6,200,"groceries")
+    println(p.product("groceries"))
+var y="Maldive"
+    println(y[2])
+    println(y[4])
+    println(y[6])
+
+
+}
+fun Even(digit:Int){
+    if(digit % 2==0){
+        println("True")
+    }else{
+        println("False")
+    }
+
+}
+data class Kibanda(var name:String,var weight:Int,var price:Int,var category:String){
+    fun product(category:String,){
+        var groceries= listOf<String>()
+        var hygiene= listOf<String>()
+        when(category){
+         "groceries"-> println("shop fruits")
+         //"hygiene"-> println("wash your hands")
+        // else-> println("closed")
+
+        }
+    }
+
+fun Country(country:String){
+    var y="Maldive"
+
+
+}
+
+}
+fun Names( name1:String,name2:String,name3:String){
+
+
+}
+
